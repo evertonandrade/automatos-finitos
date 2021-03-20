cb8d6ea4e2ee4033c0c31243ced5647ca5db5886
Everton Andrade <evertonfa7@gmail.com>
2021-03-20 19:00:52 -0300

    criada função para imprimir tabela de transições

--- main.py
+++ main.py
@@ -4,11 +4,25 @@ Q = set(input("Quais são os estados do autômato? ").split(' '))
 Sigma = set(input("Qual o alfabeto do autômato? ").split(' '))
 
 delta = dict()
+data = []
 for q in list(Q):
+    linha = []
     for e in list(Sigma):
-        delta[(q, e)] = input(f'Estando em {q} e lendo {e} vai para? ')
+        valor = input(f'Estando em {q} e lendo {e} vai para? ')
+        delta[(q, e)] = valor
+        linha.append(valor)
+    data.append(linha)
 
-print(delta)
+
+def print_table(linhas, colunas, dados):
+    format_row = "{:>4}" * (len(colunas) + 1)
+    print(format_row.format("    |", *colunas))
+    print(format_row.format("----+", *['-----' for e in colunas]))
+    for team, row in zip(linhas, dados):
+        print(format_row.format(f' {team} |', *row))
+
+
+print_table(Q, Sigma, data)
 
 q0 = input('qual o estado inicial? ')
 F = input("Quais são os estados finais? ").split(' ')
