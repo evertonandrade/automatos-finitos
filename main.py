aa57afedc0369090bb45feee1e7a7971c5177b08
Everton Andrade <evertonfa7@gmail.com>
2021-03-21 09:59:03 -0300

    fixing

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from automato import afd, afn
 
-'''
+
 def subdividir_lista(lista, n):
     lista_subdivida = []
     for i in range(0, len(lista), n):
-        lista_subdivida.append(lista[i:i + n])
+        linha = [str(d) for d in lista[i:i + n]]
+        lista_subdivida.append(linha)
     return lista_subdivida
 
 
@@ -16,6 +17,14 @@ for q in list(Q):
     for e in list(Sigma):
         delta[(q, e)] = input(f'Estando em {q} e lendo {e} vai para? ')
 
+# delta = {
+#     ('q1', '0'): {'q1', 'q2'},
+#     ('q1', '1'): {'q1'},
+#     ('q2', '0'): {'q3'},
+#     ('q2', '1'): {'q3'},
+#     ('q3', '0'): {'q4'},
+#     ('q3', '1'): {'q4'},
+# }
 
 dados = subdividir_lista(list(delta.values()), len(Sigma))
 
@@ -26,8 +35,8 @@ def print_table(linhas, colunas, dados):
     format_row = "{:>4}" * (len(colunas) + 1)
     print(format_row.format("    |", *colunas))
     print(format_row.format("----+", *['-----' for e in colunas]))
-    for team, row in zip(linhas, dados):
-        print(format_row.format(f' {team} |', *row))
+    for linha, dados_linha in zip(linhas, dados):
+        print(format_row.format(f' {linha} |', *dados_linha))
 
 
 print_table(Q, Sigma, dados)
@@ -44,7 +53,12 @@ cadeia = input("Informe a Cadeia: ")
 
 print("A cadeia foi aceita? ", end='')
 print(afd(Q, Sigma, delta, 'q0', F, cadeia))
+
+
 '''
+
+Q = ['q1', 'q2', 'q3', 'q4']
+Sigma = ['0', '1']
 
 delta = {
     ('q1', '0'): {'q1'},
@@ -55,6 +69,27 @@ delta = {
     ('q3', '1'): {'q4'},
 }
 
-a = afn(['q1', 'q2', 'q3', 'q4'], ['0', '1'],
-        delta, 'q1', {'q4'}, '01010101011110111')
+
+def subdividir_lista(lista, n):
+    lista_subdivida = []
+    for i in range(0, len(lista), n):
+        lista_subdivida.append(lista[i:i + n])
+    return lista_subdivida
+
+
+dados = subdividir_lista(list(delta.values()), len(Sigma))
+
+print(dados)
+
+
+print_table(Q, Sigma, dados)
+
+F = {'q4'}
+if F.issubset(Q):
+    print('Ok')
+else:
+    print('Erro')
+
+a = afn(Q, Sigma, delta, 'q1', F, '01010101011110111')
 print(a)
+'''
